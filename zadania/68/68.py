d07d1ce2d37ced330313188376335dc576ac298b
bartek <bartosz.nowosad@infotech.edu.pl>
2025-02-25 12:48:11 +0100

    plik cos zrobilem 68

--- zadania/68/68.py
+++ zadania/68/68.py
@@ -1,5 +1,29 @@
+def jednolity(napis):
+    for i in range(len(napis) - 1):
+        if napis[i] != napis[i + 1]:
+            return False
+    return True
+
 lista_napisow = []
 with open("C:/Users/bar.nowosad/Desktop/visual/zadania/68/dane_napisy.txt") as plik:
     for linia in plik:
         lista_napisow.append(linia.split())
-print(lista_napisow)
+# print(lista_napisow)
+
+licznik = 0
+for napisy in lista_napisow:
+    if napisy[0] == napisy[1]:
+        if jednolity(napisy[0]):
+            licznik += 1
+print(licznik)
+
+licznik = 0
+for napisy in lista_napisow:
+    if sorted(napisy[0]) == sorted(napisy[1]):
+        licznik += 1
+print(licznik)
+
+licznik = 0
+for napisy in lista_napisow:
+    napisy.sort()
+
