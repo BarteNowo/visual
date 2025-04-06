20a003b9a4c8617423f5c5b35db98ff4e3b96290
Bartek <bartosz.nowosad@infotech.edu.pl>
2025-04-06 13:38:11 +0200

    76

--- zadania/76/76.py
+++ zadania/76/76.py
@@ -1,2 +1,30 @@
-def szyfr(tekst, lista_szyfru):
-    zaszyfrowany = ""
+def szyfruj_przestawieniowo(tekst, klucz):
+    tekst = list(tekst)
+    n = len(klucz)
+    for i in range(len(tekst)):
+        poz_klucza = i % n
+        poz_w_tekscie = klucz[poz_klucza] - 1
+        if poz_w_tekscie < len(tekst):
+            tekst[i], tekst[poz_w_tekscie] = tekst[poz_w_tekscie], tekst[i]
+    
+    return ''.join(tekst)
+
+tekst = "maturainformatykaalgorytmystrukturydanychzlozonosc"
+klucz = [13, 10, 2, 3, 1, 12, 8, 4, 5, 7, 9, 6, 15, 14, 11]
+zaszyfrowane = szyfruj_przestawieniowo(tekst, klucz)
+print(f"Zaszyfrowane: {zaszyfrowane}")
+
+def deszyfruj_przestawieniowo(zaszyfrowany, klucz):
+    tekst = list(zaszyfrowany)
+    n = len(klucz)
+    for i in range(len(tekst) - 1, -1, -1):
+        poz_klucza = i % n
+        poz_w_tekscie = klucz[poz_klucza] - 1
+        if poz_w_tekscie < len(tekst):
+            tekst[i], tekst[poz_w_tekscie] = tekst[poz_w_tekscie], tekst[i]
+    return ''.join(tekst)
+
+zaszyfrowane = "rmtauanszoocolzhcynadyrutkurtsymtyroglaakytamrofni"
+klucz = [6, 2, 4, 1, 5, 3]
+odszyfrowane = deszyfruj_przestawieniowo(zaszyfrowane, klucz)
+print(f"Odszyfrowane: {odszyfrowane}")
